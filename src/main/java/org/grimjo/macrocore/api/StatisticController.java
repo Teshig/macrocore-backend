116d2defd60c91d41be77b8a7b1dcca36b66731c
teshi <teshignev@gmail.com>
2025-11-30 01:38:09 +0100

    MBE-1 Initial back end

---
+++ src/main/java/org/grimjo/macrocore/api/StatisticController.java
@@ -0,0 +1,15 @@
+package org.grimjo.macrocore.api;
+
+import lombok.Builder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Builder
+@RestController
+public class StatisticController {
+
+  @GetMapping("/")
+  public String statistic() {
+    return "Hello from Fly.io StatisticController!";
+  }
+}
